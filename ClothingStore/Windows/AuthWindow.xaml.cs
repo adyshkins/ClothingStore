b4a9719d6759a96ac99e55caff475d97909f24ab
Сергей Адышкин <adyshkinss@outlook.com>
2023-01-30 09:28:39 +0300

    Добавьте файлы проекта.

---
+++ ClothingStore/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static ClothingStore.ClassHelper.EFClass;
+
+namespace ClothingStore.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            var userAuth = Context.User.ToList()
+                .Where(i => i.Login == TbLogin.Text && i.Password == PbPassword.Password)
+                .FirstOrDefault();
+            if (userAuth != null)
+            {
+                // go to 
+                MessageBox.Show("OK");
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+    }
+}
