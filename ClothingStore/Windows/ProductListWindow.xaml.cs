153ff3562c9adf1a1bd684d15a214651a0172e96
Сергей Адышкин <adyshkinss@outlook.com>
2023-02-09 11:52:53 +0300

    List Product

---
+++ ClothingStore/Windows/ProductListWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+using ClothingStore.ClassHelper;
+using ClothingStore.Windows;
+using ClothingStore.DB;
+
+namespace ClothingStore.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListWindow.xaml
+    /// </summary>
+    public partial class ProductListWindow : Window
+    {
+        public ProductListWindow()
+        {
+            InitializeComponent();
+
+            GetListProduct();
+        }
+
+        private void GetListProduct()
+        {
+            List<Product> products = new List<Product>();
+            products = EFClass.Context.Product.ToList();
+
+            LvProduct.ItemsSource = products;
+        }
+    }
+}
